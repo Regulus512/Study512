4b1944d315db0b84df51b8c693a79d9dc344a1c8
Regulus512 <minji010402@gmail.com>
2024-12-27 23:59:57 +0900

    GameKit- Generic Method (Manager) test setup

---
+++ UnityGames/GameKit/Assets/Scripts/GameMng.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Test Class (일반화 함수를 호출할 클래스)
+public class GameMng : MonoBehaviour
+{
+    public GameObject[] ManagerObjs;
+
+}
---
+++ UnityGames/GameKit/Assets/Scripts/Mng1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Test Class (일반화 함수 호출로 불러올 클래스)
+public class Mng1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
