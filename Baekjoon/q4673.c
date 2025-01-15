71c0ce137a3894531aee96a4b8d32a713dbbed6a
Regulus512 <minji010402@gmail.com>
2025-01-15 15:01:44 +0900

    Baekjoon- q4673 (solved)

--- Baekjoon/q4673.c
+++ Baekjoon/q4673.c
@@ -4,19 +4,33 @@ int cnt = 0; // 최대 얼마나 나오는지 확인
 
 int d(int n)
 {
-	cnt++;
+	//printf("d(%d)=", n);
 	int res = n;
 	while(n)
 	{
+		cnt++;
 		res += n % 10;
 		n /= 10;
 	}
+	//printf("%d\n", res);
 	return res;
-	return res;
+}
+
+int selfnum(int n)
+{
+	for(int i=1; i<n; i++)
+	{
+		if(d(i)==n) return 0;
+	}
+	return 1;
 }
 
 int main()
 {
-	int n = 9993;
-	printf("%d", d(n));
+	for(int i = 1; i<=10000; i++)
+	{
+		if (selfnum(i)) printf("%d\n", i);
+		//printf("%d) %d\n", i, selfnum(i));
+	}
+	printf("cnt: %d\n", cnt);
 }
