46b35f2add1ad6553684fe9fb18da449f4465a80
regulus512 <minji010402@gmail.com>
2025-01-16 15:33:52 +0900

    Baekjoon- q1065

---
+++ Baekjoon/q1065.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int getdif(int n)
+{
+    if (n == 0) return 1;
+    int res = n % 10 - n / 10 % 10;
+    //printf("%d - %d = %d\n", n%10, n/10%10, res);
+    return res;
+}
+
+int main()
+{
+    int x = 9;
+    if (x == 1000) x--;
+    int cnt = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        int dif = getdif(i);
+        if (dif == getdif(i / 10))
+        {
+            printf("%d true\n", i);
+            cnt++;
+        }
+        else
+        {
+            //printf("%d false\n", i);
+        }
+    }
+    
+    printf("%d", cnt);
+}
+
