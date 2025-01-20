90d67e91552ae76c9745a5a33c9dbcab8eae3805
Regulus512 <minji010402@gmail.com>
2025-01-20 21:45:52 +0900

    Baekjoon- q10837 (solved)

--- Baekjoon/10837.c
+++ Baekjoon/10837.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 
-int cnt = 0;
 int game(int k, int m, int n)
 {
 	int s1=0, s2=0;
 	while(k--)
 	{
 		s1 += (s1 < m) ? 1 : 0;
-		cnt++;
 		if (s1 + k < s2) break;
-
+		
 		s2 += (s2 < n) ? 1 : 0;
-		cnt++;
 		if (s2 + k < s1) break;
 		
-
 	}
 	return (s1 == m && s2 == n)? 1 : 0;
 }
 
 int main()
 {
-	int k=1000, n=100000;
+	int k, n; scanf_s("%d %d", &k, &n);
 	while(n--)
 	{
-		int m=1000, n=1000;
-		game(k, m, n);
+		int m, n; scanf_s("%d %d", &m, &n);
+		printf("%d\n", game(k, m, n));
 	}
-	printf("cnt: %d", cnt);
 }
