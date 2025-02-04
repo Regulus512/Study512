f766c70c805283224b63e48c4614425b7de71e2a
Regulus512 <minji010402@gmail.com>
2025-02-04 23:23:06 +0900

    Baekjoon- q2004 (v2, v5)

--- Baekjoon/q2004.c
+++ Baekjoon/q2004.c
@@ -1,23 +1,45 @@
 #include<stdio.h>
-long long nCr(int n, int r)
+int nCr(int n, int r)
 {
 	int x = (n - r < r) ? r : n-r;
 	int y = (n - r < r) ? n-r : r;
-	long long a=1,b=1;
+
+	int v2 = 0, v5 = 0;
 	for (int i = n; i > x; i--)
 	{
 		printf("%d ", i);
-		a *= i;
+		int v = i;
+		while(v%2==0)
+		{
+			v /= 2;
+			v2++;
+		}
+		while(v%5==0)
+		{
+			v /= 5;
+			v5++;
+		}
 	}
 	printf("\n");
 	for (int i = y; i > 0; i--)
 	{
 		printf("%d ", i);
-		b *= i;
+		int v = i;
+		while(v%2==0)
+		{
+			v /= 2;
+			v2--;
+		}
+		while(v%5==0)
+		{
+			v /= 5;
+			v5--;
+		}
 	}
-
-	printf("\n%dC%d = %lld/%lld = %lld",n, r, a, b, a/b);
-	return a/b;
+	printf("\n");
+	printf("v2)%d v5) %d\n", v2, v5);
+	
+	return (v2<v5)? v2 : v5;
 }
 
 int Count(long long v)
@@ -32,12 +54,14 @@ int Count(long long v)
 
 int main()
 {
-	int n = 25;
-	for(int i = 1; i<=n; i++)
+	printf("%d", nCr(100, 98));
+
+	//int n = 25;
+	/*for(int i = 1; i<=n; i++)
 	{
 		long long res = nCr(n, i);
 
 		printf("'s cnt: %d\n\n", Count(res));
-	}
+	}*/
 		
 }
