5f6087ee14aea59536903ef797131e9f89369a7f
Regulus512 <minji010402@gmail.com>
2025-02-07 15:08:27 +0900

    Baekjoon- q2004 (v2 +2, v5 +5)

--- Baekjoon/q2004.c
+++ Baekjoon/q2004.c
@@ -1,67 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int nCr(int n, int r)
 {
 	int x = (n - r < r) ? r : n-r;
 	int y = (n - r < r) ? n-r : r;
 
 	int v2 = 0, v5 = 0;
-	for(int i=5; i<=n;i+=5)
-	{
-		printf("%d, ", i);
-		if (i <= x) continue;
 
-		int v = i;
-		while(v%5==0)
-		{
-			v5++; v /= 5;
-		}
-		
-	}
-	//printf("[v5: %d]\n", v5);
-	printf(" / ");
-	for(int i=5; i<= y; i+=5)
+	int a=0, b=0;
+	for(int i=5; i<=n; i+=5)
 	{
-		printf("%d, ", i);
-		int v = i;
+		if (i <= y)
+		{
+			int v = i;
 			while(v%5==0)
 			{
-				v5--; v /= 5;
+				a++; v /= 5;
 			}
+			
+		}
+		else if (i > x)
+		{
+			int v = i;
+			while(v%5==0)
+			{
+				b++; v /= 5;
+			}
+		}
 	}
-	printf("\n");
+	v5 = b - a;
 	
-	x = (x % 2 == 0) ? x : x + 1;
-	for(int i=n; i>x; i-=2)
+	a=0; b=0;
+	for(int i=2; i<=n; i+=2)
 	{
-		int cnt = 0;
-		int v = i;
-		while(v%2==0)
+	    if (i <= y)
 		{
-			v2++; cnt++;
-			v /= 2;
+			int v = i;
+			while(v%2==0)
+			{
+				a++; v /= 2;
+			}
+			
 		}
-		printf("%d ", cnt);
-	}
-	printf("\n");
-	printf("[v2: %d]", v2);
-	printf(" / ");
-	for(int i=2; i<=y; i+=2)
-	{
-		int cnt = 0;
-		int v = i;
-		while(v%2==0)
+		else if (i > x)
 		{
-			v2--; cnt++;
-			v /= 2;
+			int v = i;
+			while(v%2==0)
+			{
+				b++; v /= 2;
+			}
 		}
-		printf("%d ", cnt);
 	}
-	printf("\n");
-	printf("[v2: %d]", v2);
-	return (v2<v5)? v2 : v5;
+	v2=b-a;
+	
+	
+	printf("[v2: %d, v5: %d]\n", v2, v5);
+	//printf("a=%d b=%d\n", a,b);
+	return v5;
 }
-
 int main()
 {
-	nCr(100, 50);
+    nCr(25, 12);
 }
