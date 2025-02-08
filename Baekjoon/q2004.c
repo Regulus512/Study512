aec010f2485da4d0ecc3dcf5509f298aa61fb599
regulus512 <minji010402@gmail.com>
2025-02-08 12:28:53 +0900

    Baekjoon- q2004 (div)

--- Baekjoon/q2004.c
+++ Baekjoon/q2004.c
@@ -7,38 +7,48 @@ int nCr(int n, int r)
 
 	int v2 = 0, v5 = 0;
 
-	int a=0, b=0;
-	for(int i=5; i<=n; i+=5)
+	for (int i = 5; i <= n; i += 5)
 	{
+		int cnt = 0;
 		if (i <= y)
 		{
+			//printf("%d: ", i);
+			//printf("-");
 			int v = i;
-			while(v%5==0)
-			{
-				a++; v /= 5;
-			}
 			
+			while (v % 5 == 0)
+			{
+				v5--; v /= 5;
+				cnt++;
+			}
+			//printf("%d ", cnt);
 		}
 		else if (i > x)
 		{
+			//printf("%d: ", i);
+			//printf("+");
 			int v = i;
-			while(v%5==0)
+			while (v % 5 == 0)
 			{
-				b++; v /= 5;
+				v5++; v /= 5;
+				cnt++;
 			}
+			//printf("%d ", cnt);
 		}
+		
 	}
-	v5 = b - a;
 	
-	a=0; b=0;
+	printf("\n");
+
 	for(int i=2; i<=n; i+=2)
 	{
+		int cnt = 0;
 	    if (i <= y)
 		{
 			int v = i;
 			while(v%2==0)
 			{
-				a++; v /= 2;
+				v2--; v /= 2;
 			}
 			
 		}
@@ -47,18 +57,41 @@ int nCr(int n, int r)
 			int v = i;
 			while(v%2==0)
 			{
-				b++; v /= 2;
+				v2++; v /= 2;
 			}
 		}
 	}
-	v2=b-a;
-	
-	
-	printf("[v2: %d, v5: %d]\n", v2, v5);
+	//printf("[v2: %d, v5: %d]\n", v2, v5);
 	//printf("a=%d b=%d\n", a,b);
-	return v5;
+	return (v2<v5)? v2 : v5;
 }
+
+int div(int a, int b)
+{
+	int cnt = 0;
+	int v = b;
+
+	while (a/v)
+	{
+		
+		cnt += a / v;
+		v *= b;
+	}
+	//printf("div(%d, %d)=%d\n", a,b,cnt);
+	return cnt;
+}
+
 int main()
 {
-    nCr(25, 12);
+	int v2 = 0, v5 = 0;
+	int n = 25, r = 12;
+	v2 = div(n, 2) - div(r, 2) - div(n - r, 2);
+	v5 = div(n, 5) - div(r, 5) - div(n - r, 5);
+
+	//printf("%d %d\n", v2, v5);
+	printf("%d\n", (v2 < v5) ? v2 : v5);
+	
+
+	
+	printf("%d\n", nCr(25, 12));
 }
