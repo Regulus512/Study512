8233ea6bbdca8378d2f6241153eb1bf051e22f69
regulus512 <minji010402@gmail.com>
2025-02-10 23:52:42 +0900

    Baekjoon- q1614 (switch)

--- Baekjoon/q1614.c
+++ Baekjoon/q1614.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    int n=5, cnt=1;
+int count(int n, int cnt)
+{
     long long res = 0;
 
-    int v=-1;
-    for(int i=1; ; i+=v, res++)
+    int v = -1;
+    for (int i = 1; ; i += v, res++)
     {
-        printf("%lld) %d",res, i);
-        if(i == n)
+        //printf("%lld) %d", res, i);
+        if (i == n)
         {
-            printf("*");
+            //printf("*");
             cnt--;
             if (cnt < 0)
             {
-                printf("\n");
+                //printf("\n");
                 break;
             }
         }
-        if(i==1 || i==5) v*=-1;
-        printf("\n");
+        if (i == 1 || i == 5) v *= -1;
+        //printf("\n");
     }
-    
-    printf("%lld", res);
+
+    printf("count: %lld\n", res);
+}
+
+
+int main() {
+    long long n=2, cnt=3;
+    long long res = 0;
+
+    switch (n)
+    {
+        case 1:
+        case 5:
+            res = 8 * cnt + (n - 1);
+            break;
+        case 2:
+        case 3:
+        case 4:
+            res = (cnt % 2 == 0) ? 4 * cnt + (n - 1) : 4 * cnt + (10 - n) - 1;
+            break;
+    }
+    printf("switch: %lld\n", res);
+    count(n, cnt);
 }
