82ec3e31397cd44a7c44d5aa757783cd50ca65d6
regulus512 <minji010402@gmail.com>
2025-02-15 13:58:44 +0900

    Baekjoon- q4948 (Eratosthenes)

--- Baekjoon/q4948.c
+++ Baekjoon/q4948.c
@@ -2,34 +2,43 @@
 
 int isPrime(int n)
 {
-	for (int i = 3; i < n; i++)
+	for (int i = 2; i < n; i++)
 	{
 		if (n % i == 0) return 0;
 	}
 	return 1;
 }
 
+int some(int n)
+{
+	for (int v = 2; v < n; v++)
+	{
+		if (!isPrime(v)) continue;
+		//printf("%d\n", v);
+		int s = n / v, e = 2 * n / v;
+		for (int i = s; i <= e; i++)
+		{
+			//printf("%d ", i * v);
+			if (i * v == n) return 0;
+		}
+		//printf("\n");
+	}
+	return 1;
+}
+
 int main()
 {
-	while (1)
-	{
-		int n; scanf_s("%d", &n);
-		if (n == 0) break;
-		else if (n == 1)
-		{
-			printf("1\n");
-			continue;
-		}
-		int cnt = 0;
-		for (int i = (n % 2 == 0) ? n + 1 : n + 2; i <= 2 * n; i += 2)
-		{
-			if (isPrime(i))
-			{
-				//printf("Prime: %d\n", i);
-				cnt++;
-			}
-		}
-		printf("%d\n", cnt);
-	}
+	int n=10; //scanf_s("%d", &n);
+	//printf("%d\n", some(n));
+	int cnt = 0;
 	
+	for (int i = n + 1; i <= 2 * n; i++)
+	{
+		if (some(i))
+		{
+			cnt++;
+			//printf("%d\n", i);
+		}
+	}
+	printf("%d\n", cnt);
 }
