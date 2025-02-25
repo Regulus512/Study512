6dcd0da8023e1619c40128d5613a44d8d7efb661
Regulus512 <minji010402@gmail.com>
2025-02-25 23:55:58 +0900

    Baekjoon- q6588 (p)

--- Baekjoon/q6588.c
+++ Baekjoon/q6588.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
-int isPrime(int n)
-{
-    if (n % 2 == 0) return 0;
-    for (int i = 3; i < n; i += 2)
-    {
-        if (n % i == 0) return 0;
-    }
-    return 1;
-}
-
 
 int isOddPrime(int n, int p)
 {
     if (n == 3) return n;
     
     //printf("p: %d\n", p);
-    for(int i=3; i<n && i<p; i+=2)
+    for(int i=3; i<n/2 && i<p; i+=2)
     {
         if (n % i == 0)
         {
             //printf("%d is divided by %d\n", n, i);
             return 0;
         }
-        i += 2;
-        /*
-        while (isPrime(i)==0)
-        {
-            i += 2;
-        }*/
-        
     }
     return n;
 }
@@ -36,10 +19,10 @@ int isOddPrime(int n, int p)
 int main()
 {
     
-    //for(int i = 0; i<100000; i++)
+    for(int i = 0; i<100000; i++)
     //for(int n=100; n<=150; n+=2)
     {
-        int n=100000; // a: 17+9999...
+        int n=1000000;
         
         int a = 0, b = 0, p = 3;
         for(; p*p<=n; p+=2);
@@ -53,8 +36,8 @@ int main()
                 break;
             }
         }
-        if (0 < a && 0 < b)
-            printf("%d = %d + %d\n", n, a, b);
+        if (0 < a && 0 < b){}
+            //printf("%d = %d + %d\n", n, a, b);
         else
         {
             printf("%d's Goldbach's conjecture is wrong.\n", n);
